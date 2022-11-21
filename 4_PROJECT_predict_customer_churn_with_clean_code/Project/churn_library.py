44025557841fd9cfb3089dd963c2a1dec430bc8c
lewi0332 <derrickjameslewis@gmail.com>
2022-11-20 20:39:45 -0600

    touchups

--- 4_PROJECT_predict_customer_churn_with_clean_code/Project/churn_library.py
+++ 4_PROJECT_predict_customer_churn_with_clean_code/Project/churn_library.py
@@ -7,33 +7,31 @@ Date: November 2022
 """
 
 # import libraries
+# from sklearn.preprocessing import normalize
 import logging
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import RocCurveDisplay, classification_report
 import pandas as pd
 import shap
 import joblib
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns; sns.set()
+import seaborn as sns
+sns.set()
 
-from sklearn.preprocessing import normalize
-from sklearn.model_selection import train_test_split
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-
-from sklearn.metrics import plot_roc_curve, classification_report
 
 os.environ['QT_QPA_PLATFORM'] = 'offscreen'
-
-#TODO should I import logging here or in the if __name__ bit?
 
 logging.basicConfig(
     filename="./results.log",
     level=logging.INFO,
     filemode='w',
-    format='%(name)s - %(levelname)s - %(message)s')
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 
 def import_data(pth, response):
@@ -52,7 +50,6 @@ def import_data(pth, response):
         logging.info('SUCCESS: Read in %d rows of data', len(dff))
         dff['CLIENTNUM'] = dff['CLIENTNUM'].astype(str)
         logging.info('SUCCESS: convered CLIENTNUM column to string.')
-        #Create encoded Churn column
     except FileNotFoundError as err:
         logging.error(err)
     try:
@@ -61,13 +58,12 @@ def import_data(pth, response):
             lambda val: 0 if val == "Existing Customer" else 1)
         dff.drop('Attrition_Flag', axis=1, inplace=True)
         logging.info("SUCCESS: created target variable.")
-        return dff
     except KeyError as err:
-        logging.error("FAIL: %(target)s is not a column in the DataFrame. %(error)s", 
-                {'target':response, 'error':err})
+        logging.error("FAIL: %(target)s not a column in DF. %(error)s",
+                      {'target': response, 'error': err})
     except Exception as err:
         logging.error("FAIL: Could not create column: %s", err)
-    
+    return dff
 
 
 def perform_eda(dff):
@@ -79,10 +75,10 @@ def perform_eda(dff):
     output:
             None
     '''
-    print(dff.head)
-    print(dff.shape)
-    print(dff.isnull().sum())
-    
+    # print(dff.head)
+    # print(dff.shape)
+    # print(dff.isnull().sum())
+
     try:
         logging.info("Attempting churn view.")
         plt.figure(figsize=(20, 10))
@@ -119,8 +115,11 @@ def perform_eda(dff):
 
     try:
         logging.info("Attempting correlation heat map view.")
-        plt.figure(figsize=(20, 10)) 
-        sns.heatmap(dff.corr(numeric_only=True), annot=False, cmap='Dark2_r', linewidths=2)
+        plt.figure(figsize=(20, 10))
+        sns.heatmap(dff.corr(numeric_only=True),
+                    annot=False,
+                    cmap='Dark2_r',
+                    linewidths=2)
         plt.savefig('./images/corr_plot.png')
     except Exception as err:  # Not sure why this could fail, leaving it open
         logging.error("FAIL: The correlation heatmap failed. \
@@ -130,13 +129,13 @@ def perform_eda(dff):
 def encoder_helper(dff, category_lst, response):
     '''
     helper function to turn each categorical column into a new column with
-    propotion of churn for each category - associated with cell 15 from the 
+    propotion of churn for each category - associated with cell 15 from the
     notebook
 
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be 
+            response: string of response name [optional argument that could be
             used for naming variables or index y column]
 
     output:
@@ -147,7 +146,8 @@ def encoder_helper(dff, category_lst, response):
         try:
             logging.info("Creating encoded column %s", category)
             temp_cat_lst = []
-            gender_groups = dff.groupby(category).mean(numeric_only=True)[response]
+            gender_groups = dff.groupby(category).mean(
+                    numeric_only=True)[response]
 
             for val in dff[category]:
                 temp_cat_lst.append(gender_groups.loc[val])
@@ -166,7 +166,7 @@ def perform_feature_engineering(dff, response):
     '''
     input:
               df: pandas dataframe
-              response: string of response name [optional argument that could 
+              response: string of response name [optional argument that could
               be used for naming variables or index y column]
 
     output:
@@ -175,27 +175,28 @@ def perform_feature_engineering(dff, response):
               y_train: y training data
               y_test: y testing data
     '''
-    CATEGORY_LST = dff.select_dtypes(object).columns
+    category_lst = dff.drop('CLIENTNUM',
+                            axis=1).select_dtypes(object).columns
 
-    dff = encoder_helper(dff, CATEGORY_LST, response=response)
+    dff = encoder_helper(dff, category_lst, response=response)
 
     try:
         logging.info("Splitting DataFrame into Features and Target...")
         target = dff.pop(response)
         features = dff.select_dtypes(include=np.number)
         logging.info('SUCCESS: Split dataframe into %d "X" features and "y" \
-            target created.', len(features.columns))
+target created.', len(features.columns))
         logging.info("Splitting data into Train and Test sets")
         x_train, x_test, y_train, y_test = train_test_split(features,
                                                             target,
                                                             test_size=0.3,
                                                             random_state=42)
         logging.info("SUCCESS: Data sets split into %(train_len)d training \
-            rows and %(test_len)d test rows.", 
-            {"train_len": len(x_train), "test_len": len(x_test)})
-        return x_train, x_test, y_train, y_test
+rows and %(test_len)d test rows.",
+                     {"train_len": len(x_train), "test_len": len(x_test)})
     except Exception as err:
         logging.error("FAIL: Could not split dataset: %s", err)
+    return x_train, x_test, y_train, y_test
 
 
 def classification_report_image(y_train,
@@ -205,7 +206,7 @@ def classification_report_image(y_train,
                                 model_type,
                                 output_pth):
     '''
-    produces classification report for training and testing results and stores 
+    produces classification report for training and testing results and stores
     report as image in images folder
     input:
             y_train: training response values
@@ -222,18 +223,18 @@ def classification_report_image(y_train,
         logging.info('Starting classification report for %s', model_type)
         plt.rc('figure', figsize=(5, 5))
         plt.text(0.01, 1.25, str(model_type + " Test"),
-                 {'fontsize': 10}, 
+                 {'fontsize': 10},
                  fontproperties='monospace')
         plt.text(0.01, 0.05, str(classification_report(y_test, y_test_preds)),
                  {'fontsize': 10}, fontproperties='monospace')
-        plt.text(0.01, 0.6, str(model_type + " Train"), {'fontsize': 10}, 
+        plt.text(0.01, 0.6, str(model_type + " Train"), {'fontsize': 10},
                  fontproperties='monospace')
-        plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds)), 
+        plt.text(0.01, 0.7, str(classification_report(y_train, y_train_preds)),
                  {'fontsize': 10}, fontproperties='monospace')
         plt.axis('off')
-        plt.savefig(output_pth + '.' + 'classification_report_' + model_type + 
+        plt.savefig(output_pth + '.' + 'classification_report_' + model_type +
                     '.png')
-        logging.info("SUCCESS: Plot for type %(model)s saved to %(path)s", 
+        logging.info("SUCCESS: Plot for type %(model)s saved to %(path)s",
                      {'model': model_type, 'path': output_pth})
     except Exception as err:
         logging.error('FAIL: could not save classification report %(model)s \
@@ -256,13 +257,15 @@ def feature_importance_plot(model, x_data, output_pth):
         logging.info("Starting importance plot")
         # Calculate feature importances
         importances = model.best_estimator_.feature_importances_
-        logging.info("SUCCESS: collected %d important features from model", len(importances))
+        logging.info("SUCCESS: collected %d important features from model",
+                     len(importances))
         # Sort feature importances in descending order
         indices = np.argsort(importances)[::-1]
-        logging.info("SUCCESS: Gathered indices of features important features")
+        logging.info("SUCCESS: Gather indices of features important features")
         # Rearrange feature names so they match the sorted feature importances
         names = [x_data.columns[i] for i in indices]
-        logging.info("SUCCCSS: Gathered %d feature names from data.", len(names))
+        logging.info("SUCCCSS: Gathered %d feature names from data.",
+                     len(names))
         # Create plot
         plt.figure(figsize=(20, 5))
 
@@ -278,7 +281,7 @@ def feature_importance_plot(model, x_data, output_pth):
         plt.savefig(output_pth)
         logging.info("SUCCESS: Plot stored at %s", output_pth)
     except Exception as err:
-        logging.error("FAIL: could not plot most important features due to: %s", err)
+        logging.error("FAIL: plot most important features: %s", err)
 
 
 def train_models(x_train, x_test, y_train, y_test):
@@ -295,13 +298,14 @@ def train_models(x_train, x_test, y_train, y_test):
     # grid search
     rfc = RandomForestClassifier(random_state=42)
     # Use a different solver if the default 'lbfgs' fails to converge
-    # Reference: https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+    # Reference:
+    # https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
     lrc = LogisticRegression(solver='lbfgs', max_iter=3000)
 
-    param_grid = { 
+    param_grid = {
         'n_estimators': [200, 500],
         # `max_features='auto'` has been deprecated in 1.1
-        'max_features': [None, 'sqrt'],
+        # 'max_features': ['auto', 'sqrt'],
         'max_depth': [4, 5, 100],
         'criterion': ['gini', 'entropy']
     }
@@ -318,11 +322,11 @@ def train_models(x_train, x_test, y_train, y_test):
     y_test_preds_lr = lrc.predict(x_test)
 
     # scores
-    classification_report_image(y_train, 
+    classification_report_image(y_train,
                                 y_test,
-                                y_train_preds_rf, 
-                                y_test_preds_rf, 
-                                'Random Forest', 
+                                y_train_preds_rf,
+                                y_test_preds_rf,
+                                'Random Forest',
                                 './images/random_forest_classif_report.png')
 
     classification_report_image(y_train,
@@ -332,15 +336,21 @@ def train_models(x_train, x_test, y_train, y_test):
                                 'Logistic Regression',
                                 './images/logit_reg_classif_report.png')
 
-    lrc_plot = plot_roc_curve(lrc, x_test, y_test)
-    rfc_disp = plot_roc_curve(cv_rfc.best_estimator_, x_test, y_test)
-
     # plots
     plt.figure(figsize=(15, 8))
-    ax = plt.gca()
-    rfc_disp.plot(ax=ax, alpha=0.8)
-    lrc_plot.plot(ax=ax, alpha=0.8)
-    plt.show()
+    axis = plt.gca()
+    RocCurveDisplay.from_estimator(lrc,
+                                   x_test,
+                                   y_test,
+                                   ax=axis,
+                                   alpha=0.8)
+    RocCurveDisplay.from_estimator(cv_rfc.best_estimator_,
+                                   x_test,
+                                   y_test,
+                                   ax=axis,
+                                   alpha=0.8)
+    # rfc_disp.plot(ax=axis, alpha=0.8)
+    # lrc_plot.plot(ax=axis, alpha=0.8)
 
     # save best model
     joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
@@ -348,6 +358,9 @@ def train_models(x_train, x_test, y_train, y_test):
 
     explainer = shap.TreeExplainer(cv_rfc.best_estimator_)
     shap_values = explainer.shap_values(x_test)
+
+    plt.figure(figsize=(15, 8))
+    axis = plt.gca()
     shap.summary_plot(shap_values, x_test, plot_type="bar", show=False)
     plt.savefig('./images/shap_explainer_summary.png')
 
@@ -361,6 +374,7 @@ def main(data_path, response):
     x_train, x_test, y_train, y_test = perform_feature_engineering(dff,
                                                                    response)
     train_models(x_train, x_test, y_train, y_test)
+    return print("Complete")
 
 
 if __name__ == "__main__":
