09527598c9bc2f81973fc37e53f270bdf0c7ad0e
lewi0332 <derrickjameslewis@gmail.com>
2022-12-10 12:14:52 -0600

    exercise 5

--- 2_BUILDING_A_REPRODUCIBLE_MODEL_WORKFLOW/3_data_exploration_and_preperation/exercise_5/starter/run.py
+++ 2_BUILDING_A_REPRODUCIBLE_MODEL_WORKFLOW/3_data_exploration_and_preperation/exercise_5/starter/run.py
@@ -4,17 +4,42 @@ import logging
 import pandas as pd
 import wandb
 
-
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
 def go(args):
 
-    run = wandb.init(project="exercise_5", job_type="process_data")
+    with wandb.init(project="exercise_5", job_type="process_data") as run:
+        logger.info("Importing parquet file from W&B")
+        artifact = run.use_artifact(args.input_artifact)
+        df = pd.read_parquet(artifact.file())
+        
+        logger.info("Dropping duplicates")
+        df.drop_duplicates().reset_index(drop=True)
+        
+        logger.info("Creating text feature")
+        df['title'].fillna(value='', inplace=True)
+        df['song_name'].fillna(value='', inplace=True)
+        df['text_feature'] = df['title'] + ' ' + df['song_name']
+        
+        logger.info("Storing artifact locally")
 
-    ## YOUR CODE HERE
-    pass
+        df.to_csv("clean_data.csv")
+
+        logger.info("Adding Artifact to Run")
+        artifact = wandb.Artifact(
+            name=args.artifact_name,
+            type=args.artifact_type,
+            description=args.artifact_description,
+        )
+        artifact.add_file("clean_data.csv")
+
+        logger.info("Logging artifact with W&B")
+        run.log_artifact(artifact)
+        
+
+
 
 
 if __name__ == "__main__":
